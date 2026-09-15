609393bbfbce6a677f2b3e881ec80aec4ee15a5e
Ivan Sumskyi <sumskiyivan627@gmail.com>
2026-09-15 12:20:37 +0000

    fix rector.php

--- rector.php
+++ rector.php
@@ -9,7 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/config',
         __DIR__ . '/public',
         __DIR__ . '/src',
-        __DIR__ . '/tests',
+        //        __DIR__ . '/tests',
     ])
     ->withPhpSets(php85: true)
     ->withPreparedSets(
